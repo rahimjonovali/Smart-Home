f90fca23eb8943c6c34e54623c33661efa3381ad
Rahimjonov Ali <rahimjonovmuhammadjon74@gmail.com>
2025-05-18 17:21:43 +0500

    changes in API view

--- mqqt_web/urls.py
+++ mqqt_web/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import turn_on, turn_off
+from .views import control_device
 
 urlpatterns = [
-    path('device/on/', turn_on, name='turn_on'),
-    path('device/off/', turn_off, name='turn_off'),
+    # path('device/on/', turn_on, name='turn_on'),
+    # path('device/off/', turn_off, name='turn_off'),
+    path('control', control_device, name='control_device'),
 ]
--- mqqt_web/views.py
+++ mqqt_web/views.py
@@ -1,14 +1,31 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .mqtt_client import send_mqtt_command
 
-@api_view(['POST'])
-def turn_on(request):
-    send_mqtt_command("ON")
-    return Response({"status": "Device turned ON"})
+# @api_view(['POST'])
+# def turn_on(request):
+#     send_mqtt_command("ON")
+#     return Response({"status": "Device turned ON"})
+
+# @api_view(['POST'])
+# def turn_off(request):
+#     send_mqtt_command("OFF")
+#     return Response({"status": "Device turned OFF"})
+
 
 @api_view(['POST'])
-def turn_off(request):
-    send_mqtt_command("OFF")
-    return Response({"status": "Device turned OFF"})
-
+def control_device(request):
+    action = request.data.get("action", "").upper()
+    
+    if action == "ON":
+        send_mqtt_command("ON")
+        return Response({"status": "Device turned ON"})
+    elif action == "OFF":
+        send_mqtt_command("OFF")
+        return Response({"status": "Device turned OFF"})
+    else:
+        return Response(
+            {"error": "Invalid action. Use 'ON' or 'OFF'."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
