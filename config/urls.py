445914acb8e173ff291a665ba06121868fb46f54
Rahimjonov Ali <rahimjonovmuhammadjon74@gmail.com>
2025-05-19 17:52:43 +0500

    Fixed settings

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,6 +21,6 @@ urlpatterns = [
                                     # Auth part
     path('login/',  auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', include('dashboard.auth_urls')),  
+    path('register/', include('dashboard.urls')),  
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/admin.py
+++ news/admin.py
@@ -3,7 +3,7 @@ from .models import NewsItem
 
 @admin.register(NewsItem)
 class NewsItemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content','created_at','updated_at',),
-    list_filter = ('title','created_at','content',),
-    search_fields = ('title', 'content',),
-    list_display_links = ('title', 'content','created_at','updated_at',),
+    list_display = ('title', 'content','created_at','updated_at')
+    list_filter = ('title','created_at','content')
+    search_fields = ('title', 'content')
+    list_display_links = ('title', 'content','created_at','updated_at')
